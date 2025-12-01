cd1d7cfe4d5936f2107370a3d92ab587ee9f4457
ThomasLawson1121 <thomaslawson1121@gmail.com>
2025-12-01 03:36:57 -0800

    first commit

---
+++ 106/scripts/dom.js
@@ -0,0 +1,16 @@
+function sayHello() {
+    console.log("Hello World");
+    howAreYou();
+}
+
+function sayGoodbye() {
+    console.log("Goodbye World");
+}
+
+function howAreYou() {
+    console.log("How are you, world?");
+    sayGoodbye();
+}
+
+window.onload = howAreYou //execute the function when the page loads
+// this means that i wait until the HTML and the css are loaded before i exxecute
